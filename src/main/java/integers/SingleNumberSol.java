5db30aa66612b771fbe6ecdc9e5c82e04f78d9a0
Franck Abdiel <foziefranck@gmail.com>
2025-11-17 05:55:49 +0100

    improving SingleNumberSolution to prompt the user

--- src/main/java/integers/SingleNumberSol.java
+++ src/main/java/integers/SingleNumberSol.java
@@ -1,9 +1,13 @@
 package integers;
 
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.logging.Logger;
 
 public class SingleNumberSol {
+
+    private static final Logger logger = Logger.getLogger(SingleNumberSol.class.getName());
 
     public int singleNumber(int[] nums) {
         if(nums.length == 1) return nums[0];
@@ -30,4 +34,20 @@ public class SingleNumberSol {
         }
         return singleNum;
     }// TC: O(n) SC: O(1)
+
+
+    public static void main(String [] args){
+        SingleNumberSol sol = new SingleNumberSol();
+        Scanner sc = new Scanner(System.in);
+        logger.info("Enter the number of elements in the array : ");
+        int n = sc.nextInt();
+        int [] nums = new int[n];
+        logger.info("Enter the elements of the array : ");
+        for(int i=0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
+        logger.info(()-> "the single number contained in your array is :"+ sol.singleNumber2(nums));
+        sc.close();
+
+    }
 }
